3eb77b02ae0a68c469ec8e85d29e9fede69f8935
userLil <141762923+us456er@users.noreply.github.com>
2024-05-24 17:03:22 +0400

    Update inputModel.py

--- inputModel.py
+++ inputModel.py
@@ -1,17 +1,34 @@
-import json                                       #pythonic JS 
+import json
 
-class inputA(object):
+class inputModel(object):
+
 	def __init__(self, input):
 		self.input = input
-		
-	def getMessage(self):
+	def getUpdateID(self):
+		return self.input['update_id']
+	def getMessageID(self):
 		return self.input['message'].get('message_id')
-
+	def getDate(self):
+		return self.input['message'].get('date')
 	def getText(self):
 		text = self.input['message'].get('text')
 		if text:
-			text = text.encode('utf-8','strict') 
+			text = text.encode('utf-8','strict')
 		return text
 		
+	def getFromID(self):
+		return self.input['message'].get('from')['id']
+	def getFromName(self):
+		return self.input['message'].get('from')['first_name']
+	def getChatID(self):
+		return self.input['message'].get('chat')['id']
 	def getLocation(self):
 		return self.input['message'].get('location')
+	def getLat(self):
+		if self.getLocation():
+			return self.getLocation().get('latitude')
+		return None
+	def getLng(self):
+		if self.getLocation():
+			return self.getLocation().get('longitude')
+		return None
